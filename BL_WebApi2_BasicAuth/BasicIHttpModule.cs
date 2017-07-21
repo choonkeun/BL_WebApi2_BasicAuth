9bb4f45165d7c0e5eaddb7c58509a82fdaf65963
choonkeun <choonkeun@yahoo.com>
2017-07-21 15:16:52 -0700

    Update

---
+++ BL_WebApi2_BasicAuth/BasicIHttpModule.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Security.Principal;
+using System.Text;
+using System.Threading;
+using System.Web;
+
+namespace BL_WebApi2_BasicAuth.Modules
+{
+    public class BasicIHttpModule : IHttpModule
+    {
+        private const string Realm = "BL_WebApi2_BasicAuth";
+
+        public void Init(HttpApplication context)
+        {
+            // Register event handlers
+            context.AuthenticateRequest += OnApplicationAuthenticateRequest;
+            context.EndRequest += OnApplicationEndRequest;
+        }
+
+        private static void OnApplicationAuthenticateRequest(object sender, EventArgs e)
+        {
+            var request = HttpContext.Current.Request;
+            var authHeader = request.Headers["Authorization"];
+            if (authHeader != null)
+            {
+                var authHeaderVal = AuthenticationHeaderValue.Parse(authHeader);
+
+                // RFC 2617 sec 1.2, "scheme" name is case-insensitive
+                if (authHeaderVal.Scheme.Equals("basic", StringComparison.OrdinalIgnoreCase) && authHeaderVal.Parameter != null)
+                {
+                    AuthenticateUser(authHeaderVal.Parameter);
+                }
+            }
+        }
+
+        private static bool AuthenticateUser(string credentials)
+        {
+            var encoding = Encoding.GetEncoding("iso-8859-1");
+            credentials = encoding.GetString(Convert.FromBase64String(credentials));
+
+            var credentialsArray = credentials.Split(':');
+            var username = credentialsArray[0];
+            var password = credentialsArray[1];
+
+            /* REPLACE THIS WITH REAL AUTHENTICATION
+            ----------------------------------------------*/
+            if (!(username == "user" && password == "pass"))
+            {
+                return false;
+            }
+
+            var identity = new GenericIdentity(username);
+            SetPrincipal(new GenericPrincipal(identity, null));
+
+            return true;
+        }
+
+        private static void SetPrincipal(IPrincipal principal)
+        {
+            Thread.CurrentPrincipal = principal;
+            if (HttpContext.Current != null)
+            {
+                HttpContext.Current.User = principal;
+            }
+        }
+
+        // If the request was unauthorized, add the WWW-Authenticate header to the response.
+        private static void OnApplicationEndRequest(object sender, EventArgs e)
+        {
+            var response = HttpContext.Current.Response;
+            if (response.StatusCode == 401)
+            {
+                response.Headers.Add("WWW-Authenticate", string.Format("Basic realm=\"{0}\"", Realm));
+                response.Status = "401 Authorization Required";
+                response.ContentType = "text/html";
+            }
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+
+
+}
